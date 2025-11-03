c2b67fc9d4f76f1c5b645efe05a79721c41d092b
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-11-03 19:39:14 +0900

    test(domain): add test for LottoNumbersPicker to verify count, range, and uniqueness

---
+++ src/test/java/lotto/domain/LottoNumbersPickerTest.java
@@ -0,0 +1,32 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LottoNumbersPickerTest {
+
+    private LottoNumbersPicker lottoNumbersPicker = new LottoNumbersPicker();
+
+    @Test
+    @DisplayName("최소값 이상 최대값 이하의 숫자를 정해진 개수만큼 뽑는다.")
+    void 최소값_이상_최대값_이하의_숫자를_정해진_개수만큼_뽑는다() {
+        // given
+        int min = 1;
+        int max = 45;
+        int count = 6;
+
+        // when
+        List<Integer> numbers = lottoNumbersPicker.pickNumbersInRange(min, max, count);
+
+        // then
+        assertThat(numbers).hasSize(count);
+        assertThat(numbers).allMatch(num -> num >= min && num <= max);
+        Set<Integer> unique = new HashSet<>(numbers);
+        assertThat(unique).hasSameSizeAs(numbers);
+    }
+}
