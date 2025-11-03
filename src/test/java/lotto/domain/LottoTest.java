c0084d085d88bcf43a711aec10df7fe33e2d3834
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-11-03 19:51:59 +0900

    test(domain): add test for Lotto#getSortedNumbers to verify ascending order

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -21,5 +22,17 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    @DisplayName("로또 번호를 오름차순으로 정렬된 상태로 반환한다.")
+    void 로또_번호를_오름차순으로_정렬된_상태로_반환한다() {
+        // given
+        Lotto lotto = new Lotto(List.of(6, 5, 4, 3, 2, 1));
+        List<Integer> expectedNumbers = List.of(1, 2, 3, 4, 5, 6);
+
+        // when
+        List<Integer> sortedNumbers = lotto.getSortedNumbers();
+
+        // then
+        assertThat(sortedNumbers).isEqualTo(expectedNumbers);
+    }
 }
