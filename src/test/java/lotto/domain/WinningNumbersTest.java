026c6f49c5a40c0d9705109c27940cbb0b411751
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-11-03 22:10:07 +0900

    test(domain): Update WinningNumbersTest to use BonusNumber

--- src/test/java/lotto/domain/WinningNumbersTest.java
+++ src/test/java/lotto/domain/WinningNumbersTest.java
@@ -12,17 +12,17 @@ class WinningNumbersTest {
     @DisplayName("보너스 번호가 당첨 번호와 중복되면 예외가 발생한다.")
     @Test
     void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 6))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(6)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("보너스 번호가 1 미만 또는 45 초과면 예외가 발생한다.")
     @Test
     void 보너스_번호가_1_미만_또는_45_초과면_예외가_발생한다() {
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 0))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(0)))
                 .isInstanceOf(IllegalArgumentException.class);
 
-        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 46))
+        assertThatThrownBy(() -> new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(46)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -30,7 +30,7 @@ class WinningNumbersTest {
     @Test
     void 로또_번호와_당첨_번호의_일치_개수를_정확히_계산한다() {
         // given
-        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(7));
         Lotto lotto = new Lotto(List.of(1, 3, 5, 7, 9, 11));
 
         // when
@@ -44,7 +44,7 @@ class WinningNumbersTest {
     @Test
     void 보너스_번호가_로또에_포함되면_bonusMatched는_true를_반환한다() {
         // given
-        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(7));
         Lotto lotto = new Lotto(List.of(7, 8, 9, 10, 11, 12));
 
         // when & then
@@ -55,7 +55,7 @@ class WinningNumbersTest {
     @Test
     void 보너스_번호가_로또에_포함되지_않으면_bonusMatched는_false를_반환한다() {
         // given
-        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), 7);
+        WinningNumbers winning = new WinningNumbers(List.of(1, 2, 3, 4, 5, 6), new BonusNumber(7));
         Lotto lotto = new Lotto(List.of(8, 9, 10, 11, 12, 13));
 
         // when & then
