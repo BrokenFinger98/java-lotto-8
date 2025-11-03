896229c297c7164ae239a50e39aae10541f9cc20
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-11-03 20:27:50 +0900

    feat(domain): Enhance Lotto validation logic for duplication, and range

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -16,6 +16,16 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        long distinctCount = numbers.stream().distinct().count();
+        if (distinctCount != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        boolean outOfRange = numbers.stream().anyMatch(num -> num < 1 || num > 45);
+        if (outOfRange) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
     public List<Integer> getSortedNumbers() {
