c4b200576c48f26a4d2abd11acff00cadb311073
BrokenFinger98 <dbtjsdn980818@gmail.com>
2025-11-03 23:32:12 +0900

    feat(app): Implement Application main flow for lotto game execution

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,50 @@
 package lotto;
 
+import java.util.List;
+import lotto.application.LottoService;
+import lotto.domain.BonusNumber;
+import lotto.domain.LottoBundle;
+import lotto.domain.LottoNumbersPicker;
+import lotto.domain.LottoResultSummary;
+import lotto.domain.Money;
+import lotto.domain.WinningNumbers;
+import lotto.presentation.InputView;
+import lotto.presentation.OutputView;
+import lotto.presentation.Parser;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoService service = new LottoService(new LottoNumbersPicker());
+
+        Money money = readMoneyLoop();
+        LottoBundle bundle = service.buyTickets(money);
+        OutputView.printPurchased(bundle);
+
+        WinningNumbers winning = readWinningLoop();
+        LottoResultSummary summary = service.summarize(bundle, winning, money);
+        OutputView.printSummary(summary);
+    }
+
+    private static Money readMoneyLoop() {
+        while (true) {
+            try {
+                String line = InputView.readMoney();
+                return new Money(Parser.parseMoney(line));
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static WinningNumbers readWinningLoop() {
+        while (true) {
+            try {
+                List<Integer> nums = Parser.parseWinningNumbers(InputView.readWinningNumbers());
+                BonusNumber bonus = new BonusNumber(Parser.parseBonus(InputView.readBonus()));
+                return new WinningNumbers(nums, bonus);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
